c9299d8f48a0a8953734c57c6fbf061364dd3daa
illushun <daveystuart1@gmail.com>
2025-10-05 23:45:44 +0100

    Forgot

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Inertia\Inertia; // Essential for the Inertia/Vue integration
+use Illuminate\Support\Facades\Log;
 
 class HomeController extends Controller
 {
