353793718d9ade5de72c93cdfef5a1874491d064
illushun <daveystuart1@gmail.com>
2025-10-13 20:38:01 +0100

    logo update

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -6,7 +6,7 @@
 
                     <div class="flex-shrink-0">
                         <a href="/" class="text-2xl font-black text-gray-900 transition hover:text-sky-600">
-                            <svg style="width:64px;" width="100%" height="100%" viewBox="0 0 500 500" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;">
+                            <svg width="100%" height="100%" viewBox="0 0 500 500" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5;width:64px;">
                                 <g id="Lighter-Purple" serif:id="Lighter Purple">
                                     <g id="Stuff">
                                         <g id="Bottom">
