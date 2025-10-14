19ca19946a3abb2e507e634d16be20d823d84a2f
illushun <daveystuart1@gmail.com>
2025-10-14 21:43:31 +0100

    missing

--- app/Http/Controllers/Product/ProductController.php
+++ app/Http/Controllers/Product/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\DB;
 
 use App\Models\Product;
 use App\Models\Product\View as ProductView;
