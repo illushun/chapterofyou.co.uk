ccc317d6ca15942451b365a2b8f5124df83b9eb5
illushun <daveystuart1@gmail.com>
2025-10-15 22:23:24 +0100

    about us

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+import { Head, router, useForm } from '@inertiajs/vue3';
+import { ref } from 'vue';
+
+import NavBar from '@/components/NavBar.vue';
+</script>
+
 <template>
 
+    <NavBar />
 
+    <Head title="About Us" />
+
+    <section class="py-20">
+
+        <div class="mx-auto max-w-screen-xl p-4 md:p-8 lg:p-12">
+
+            <div class="max-w-4xl mx-auto">
+
+            </div>
+
+        </div>
+
+    </section>
 
 </template>
