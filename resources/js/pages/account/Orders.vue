1889b2f5722e28c2dcea08089434403a9fc612fc
illushun <daveystuart1@gmail.com>
2025-10-29 19:57:47 +0000

    fix layout

--- resources/js/pages/account/Orders.vue
+++ resources/js/pages/account/Orders.vue
@@ -82,7 +82,7 @@ const hasOrders = computed(() => currentOrders.value.length > 0);
 
                 <!-- Main Content Card -->
                 <div class="rounded-xl border-2 border-copy bg-[var(--primary-content)] shadow-xl">
-                    <div class="relative rounded-xl -m-0.5 border-2 border-copy bg-foreground p-4 flex flex-col sm:flex-row items-start gap-4">
+                    <div class="relative rounded-xl -m-0.5 border-2 border-copy bg-foreground p-4">
 
                         <div v-if="!hasOrders" class="text-center py-12">
                             <div class="text-copy-light mx-auto mb-4" v-html="IconBox"></div>
