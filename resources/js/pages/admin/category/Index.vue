164ecfd7932fb84cdec665a3ea2a064ca4ff2256
illushun <daveystuart1@gmail.com>
2025-11-13 20:32:58 +0000

    i cant

--- resources/js/pages/admin/category/Index.vue
+++ resources/js/pages/admin/category/Index.vue
@@ -18,7 +18,7 @@ interface CategoriesPaginated {
 }
 
 const props = defineProps<{
-    orders: CategoriesPaginated;
+    categories: CategoriesPaginated;
 }>();
 
 const formatDate = (dateString: string): string => {
