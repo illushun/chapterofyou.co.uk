8c26a6bdd55bfaa8ef6d7eb60606f0d65c4e2f15
illushun <daveystuart1@gmail.com>
2025-11-23 14:59:17 +0000

    add use statement

--- app/Http/Controllers/Admin/AdminCourierController.php
+++ app/Http/Controllers/Admin/AdminCourierController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Courier;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class AdminCourierController extends Controller
 {
