ba1f29e60075f393a9de0ada18e8df82e769d050
illushun <daveystuart1@gmail.com>
2025-11-24 18:28:04 +0000

    add necessary includes

--- app/Http/Controllers/Admin/AdminCategoryController.php
+++ app/Http/Controllers/Admin/AdminCategoryController.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\Category;
-use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
 
 class AdminCategoryController extends Controller
 {
