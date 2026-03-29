f206fbb4491b214bc7a9e177880bcb5764df72d8
illushun <daveystuart1@gmail.com>
2026-03-29 20:34:09 +0100

    missing use statement

--- app/Http/Controllers/Cart/CheckoutController.php
+++ app/Http/Controllers/Cart/CheckoutController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Cart;
 
 use App\Http\Controllers\Controller;
+use App\Models\Cart\Item;
 use App\Services\CartManager;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
