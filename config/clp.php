65fde94a1e9cffeb990f7ac4d523d9ab654f8cc5
illushun <daveystuart1@gmail.com>
2026-03-31 13:16:29 +0100

    Add clp config

---
+++ config/clp.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'supplier_name'    => env('CLP_SUPPLIER_NAME', ''),
+    'supplier_address' => env('CLP_SUPPLIER_ADDRESS', ''),
+    'supplier_phone'   => env('CLP_SUPPLIER_PHONE', ''),
+];
