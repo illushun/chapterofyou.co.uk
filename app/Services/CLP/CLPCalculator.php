0d2cc6627566c839912e123edba01b02b8321801
illushun <daveystuart1@gmail.com>
2026-03-31 13:13:02 +0100

    forgot

--- app/Services/CLP/CLPCalculator.php
+++ app/Services/CLP/CLPCalculator.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace App\Services;
+namespace App\Services\CLP;
 
 use App\Models\Product;
 
-class ClpCalculatorService
+class CLPCalculator
 {
     // -------------------------------------------------------------------------
     // Main entry point
